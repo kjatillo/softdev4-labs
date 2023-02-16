b89586c7e8ff133661519dc1ce7a54cc53337983
Ken <keneithatillo@gmail.com>
2023-02-16 21:46:01 +0000

    modified ex6

--- Lab7/src/ex6/Ex6.java
+++ Lab7/src/ex6/Ex6.java
@@ -12,29 +12,26 @@ import java.util.Scanner;
 public class Ex6 {
     public static void main(String[] args) {
         // Declare variables
-        int[] numsArray1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        int[] numsArray2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int[] numsArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         int answer;
         int correct = 0;
         int wrong = 0;
         int total = 0;
-        int randomNum1;
-        int randomNum2;
+        int randomNum;
         // Scanner
         Scanner input = new Scanner(System.in);
 
         // Loop until 3 wrong answers
         while (wrong < 3) {
             // Generate random numbers
-            randomNum1 = (int) (Math.random() * 12);
-            randomNum2 = (int) (Math.random() * 12);
+            randomNum = (int) (Math.random() * 12);
 
             // Ask question
-            System.out.print("What is " + numsArray1[randomNum1] + " + " + numsArray2[randomNum2] + "? ");
+            System.out.print("What is " + 1 + " + " + numsArray[randomNum] + "? ");
             answer = input.nextInt();
 
             // Check answer
-            if (answer == numsArray1[randomNum1] + numsArray2[randomNum2]) {
+            if (answer == 1 + numsArray[randomNum]) {
                 System.out.println("Correct!");
                 correct++;
             } else {
